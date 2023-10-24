195168bf03eee23926f97e27ca7a10d47f0fdea3
ivan-ivano <ivan.ivano.ri.2023@lpnu.ua>
2023-10-24 19:06:31 +0300

    1

--- LAB5_1.py
+++ LAB5_1.py
@@ -2,8 +2,8 @@ def h(x, y):
     return x * y / (1 + x ** 2 * y ** 2)
 
 
-s = 2
-t = 3
+s = float(input("s: "))
+t = float(input("t: "))
 
 result = (h(s ** 2, t ** 2) + (h(s + t, 1) ** 2)) / (1 + h(s * t, 2) ** 2)
 
